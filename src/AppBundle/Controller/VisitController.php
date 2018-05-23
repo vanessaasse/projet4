3e0c3dd538b5ba2c64bc1a842b6fcedfc6db1ff5
Vanessa Asse <vanessa.asse@gmail.com>
2018-05-23 17:22:47 +0200

    [PHP]Création du controller VisitController, définition des méthodes à développer et des routes associées.

---
+++ src/AppBundle/Controller/VisitController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+class VisitController extends Controller
+{
+    /*
+     * page 1 presentation
+     *
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        //
+    }
+
+
+    /*
+     * page 2 choix des billets
+     *
+     * @Route("/order")
+     */
+    public function orderAction()
+    {
+        //
+    }
+
+
+    /*
+     * page 3 identification des visiteurs
+     *
+     * @Route("/identification")
+     */
+    public function identifyAction()
+    {
+        //
+    }
+
+
+    /*
+     * page 4 coordonnees de l'acheteur
+     *
+     * @Route("/customer")
+     */
+    public function customerAction()
+    {
+        //
+    }
+
+
+    /*
+     * page 5 paiement
+     *
+     * @Route("/pay")
+     */
+    public function payAction()
+    {
+        //
+    }
+
+
+    /*
+     * page 6 confirmation
+     *
+     * @Route("/confirmation")
+     */
+    public function confirmationAction()
+    {
+        //
+    }
+
+
+    /*
+     * page contact
+     *
+     * @Route("/contact")
+     */
+    public function contactAction()
+    {
+        //
+    }
+
+}
