32d357a12b99347d317792179e7d502c4ebc5254
Vanessa Asse <vanessa.asse@gmail.com>
2018-05-23 15:30:29 +0200

    configuration de la BDD et suppression des fichiers installés par défaut

---
+++ TODO.php
@@ -0,0 +1,20 @@
+<?php
+
+/* TODO 1 Bootstrap
+pour installer Bootstrap 4, faut-il :
+yarn add bootstrap-sass --dev
+ou
+yarn add bootstrap@4.0.0 https://www.youtube.com/watch?v=BNrrCO5seg4
+*/
+
+/* TODO 2 création d'entite
+- Dans l'entité Visit, j'ai indiqué que l'attribut bookingCode était unique. Est-ce ok ?
+
+*/
+
+
+/* TODO 3
+ * Quel est l'équivalent de php bin/console doctrine:generate:entities AppBundle:Visit
+ * avec make
+ * make:entity --regenerate
+ */
