3b1b43ad98b43c0efc1d6a834a3fbffb47516e68
Vanessa Asse <vanessa.asse@gmail.com>
2018-05-26 15:43:41 +0200

    [PHP]Création de la méthode orderAction dans VisitController

--- src/AppBundle/Controller/VisitController.php
+++ src/AppBundle/Controller/VisitController.php
@@ -2,7 +2,8 @@
 
 namespace AppBundle\Controller;
 
-
+use AppBundle\Entity\Visit;
+use AppBundle\Form\VisitType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,16 +23,49 @@ class VisitController extends Controller
 
 
     /**
-     * page 2 choix des tickets - Initialisation de la visite
+     * Initialisation de la visite
+     * page 2 choix des tickets
      *
      * @Route("/order")
      */
     public function orderAction(Request $request)
     {
-        //
-        $visit = $request->getSession()->get('visit');
-        //
+        //On crée un nouvel objet Visit
+        $visit = new visit;
 
+        //On appelle le formulaire VisitType
+        $formBuilder = $this->get('form.factory')->createBuilder(VisitType::class, $visit);
+
+        //A partir du formulaire, on le génère
+        $form = $formBuilder->getForm();
+
+        //Si la requête est en POST
+        if($request->isMethod('POST'))
+        {
+            //On fait le lien requête->formulaire
+            // Désormais, la variable $visit contient les valeurs entrées par le visiteur
+            $form->handleRequest($request);
+
+            //On vérifie que les données entrées sont valides
+            if($form->isValid())
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist();
+                $em->flush();
+
+                $request->getSession();
+
+                //On redirige l'acheteur vers la page 3 - identification des visiteurs
+                return $this->redirectToRoute('app_visit_identify');
+            }
+        }
+
+        //On est en GET. On affiche le formulaire
+        return $this->render('Visit/order.html.twig', array('form'=>$form->createView()));
+
+        //
+        //$visit = $request->getSession()->get('visit');
+        //
 
     }
 
