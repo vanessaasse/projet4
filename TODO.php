1f3f19dd435e0a193e6fada6ccda239f5ee1c7c3
Vanessa Asse <vanessa.asse@gmail.com>
2018-05-30 13:29:38 +0200

    Mise en place du template Bootstrap 4

--- TODO.php
+++ TODO.php
@@ -5,6 +5,7 @@
  * Voir le fichier config/packages/maker.yml
  * Il me semble que j'ai mal paramétré le fichier
  * les commandes make:form ne fonctionnent pas.
+ * Message d'erreur : It looks like your app may be using a namespace other than "App".
  */
 
 /* TODO 2
@@ -15,3 +16,22 @@
  * sont à mettre dans le dernier formulaire ?
  */
 
+
+/* TODO 3
+ * Datepicker
+ *  <!--<script src="{{ asset('assets/bootstrap2-datepicker-master/js/bootstrap2-datepicker.js') }}"></script>-->
+ */
+
+
+/*
+ * TODO 4
+ * Page order.htlm.twig
+ * Comment mettre une valeur par défaut dans un champs Radio ?
+ */
+
+
+/*
+ * TODO 5
+ * Page identify.html.twig
+ * Comment modifier la date d'anniversaire par défaut ?
+ */
