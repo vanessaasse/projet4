1758db33adb27eee82a8ffd6937e64985a40946f
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-08 18:50:11 +0200

    [PHP]Création d'une exception en cas de session invalide

---
+++ src/AppBundle/Exception/InvalidVisitSessionException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vanessa
+ * Date: 08/06/2018
+ * Time: 12:06
+ */
+
+namespace AppBundle\Exception;
+
+
+class InvalidVisitSessionException extends \Exception
+{
+
+
+}
