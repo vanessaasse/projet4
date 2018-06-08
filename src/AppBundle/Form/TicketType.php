1b8ef43653f5eece7b9817fb77af13c3b193240a
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-08 18:42:18 +0200

    [PHP]Ajout des required true or false, ajout de l'option "'preferred_choices' => array('FR')" dans Country

--- src/AppBundle/Form/TicketType.php
+++ src/AppBundle/Form/TicketType.php
@@ -20,16 +20,27 @@ class TicketType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('lastname', TextType::class, array(
-            'label' => 'Nom :'))
-            ->add('firstname', TextType::class, array('label' => 'Prénom :'))
-            ->add('country', CountryType::class, array('label' => 'Pays :'))
-            ->add('birthday', BirthdayType::class, array('label' => 'Date de naissance :'))
+            'label' => 'Nom*',
+            'required' => true))
+
+            ->add('firstname', TextType::class, array(
+                'label' => 'Prénom*',
+                'required' => true))
+
+            ->add('country', CountryType::class, array(
+                'label' => 'Pays*',
+                'preferred_choices' => array('FR'),
+                'required' => true))
+
+            ->add('birthday', BirthdayType::class, array(
+                'label' => 'Date de naissance*',
+                'required' => true))
+
             ->add('discount', CheckboxType::class, array(
                 'label' => 'Tarif réduit de 10 euros 
             - Tarif accordé sous certaines conditions (étudiant, employé du musée, d’un service du Ministère de la Culture, militaire…)',
                 'required' => false
-            ))
-            ->add('save', SubmitType::class, array('label' => 'Validez'));
+            ));
     }
 
 
