57465e6c112f65b92a33168471abd8b4388fb82c
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-10 16:32:30 +0200

    [PHP]Création d'une méthode getCurrentCustomer

--- src/AppBundle/Manager/CustomerManager.php
+++ src/AppBundle/Manager/CustomerManager.php
@@ -6,9 +6,26 @@ use AppBundle\Entity\Customer;
 
 class CustomerManager
 {
+
+    /**
+     * @return Customer
+     */
     public function initCustomer()
     {
         $customer = new Customer();
         return $customer;
     }
+
+
+    /**
+     * @param Customer $customer
+     */
+    public function getCurrentCustomer(Customer $customer)
+    {
+        $customer->getLastname();
+        $customer->getFirstname();
+        $customer->getEmail();
+    }
+
+
 }
