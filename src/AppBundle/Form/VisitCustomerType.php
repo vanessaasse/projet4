d5ec79980d01a495ed830cccbad2a244ddea8b64
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-11 08:33:51 +0200

    [PHP] Création de VisitCustomerType avec Customer comme EntityType

---
+++ src/AppBundle/Form/VisitCustomerType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Customer;
+use AppBundle\Entity\Visit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VisitCustomerType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('customer',EntityType::class, array(
+            'class' => Customer::class,
+            'choice_label' => 'customer',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Visit::class
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_visit_customer_type';
+    }
+}
