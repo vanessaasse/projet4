0aa887bb92125eb22f351af9875cdf2b6f1b82dc
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-13 16:08:53 +0200

    [PHP] Création de la méthode CreateValidation + début de création de la méthode priceTicket pour obtenir le prix en fonction de l'age

--- src/AppBundle/Manager/VisitManager.php
+++ src/AppBundle/Manager/VisitManager.php
@@ -55,7 +55,7 @@ class VisitManager
      */
     public function generateTickets(Visit $visit)
     {
-        for($i=0; $i<=$visit->getNbTicket(); $i++)
+        for($i= 1; $i<=$visit->getNbTicket(); $i++)
         {
             $visit->addTicket(new Ticket());
         }
@@ -77,9 +77,76 @@ class VisitManager
      */
     public function createCustomer(Visit $visit)
     {
-        $customer = new Customer;
-
-        $visit->setCustomer($customer);
+        $visit->getCustomer();
     }
+
+
+    /**
+     * @param Visit $visit
+     *
+     */
+    public function createValidation(Visit $visit)
+    {
+        $visit->getVisitDate();
+        $visit->getType();
+        $visit->getCustomer();
+        $visit->getNbTicket();
+        $visit->getTickets();
+
+    }
+
+
+    /**
+     * @param Visit $visit
+     * @param Ticket $ticket
+     * @return int
+     */
+    public function priceTicket(Visit $visit, Ticket $ticket)
+    {
+        $birthday = $ticket->getBirthday();
+        $today = new \DateTime();
+        $age = date_diff($birthday, $today);
+        $age->format('%R%y years');
+
+        if ($visit->getType() == 'Billet journée')
+        {
+            if ($age >= '12y' || $age < '60y') {
+                $price = 16;
+            } elseif ($age >= '60y') {
+                $price = 12;
+            } elseif ($age >= '4y' && $age < '12y') {
+                $price = 8;
+            } else {
+                $price = 0;
+            }
+
+            return $price;
+        }
+
+        else {
+
+            // si le billet est sur une demi-journée
+
+            if ($age >= '12y' || $age < '60y') {
+                $price = 8;
+            } elseif ($age >= '60y') {
+                $price = 6;
+            } elseif ($age >= '4y' && $age < '12y') {
+                $price = 4;
+            } else {
+                $price = 0;
+            }
+
+            return $price;
+        }
+    }
+
+
+    public function totalPriceVisit(Visit $visit)
+    {
+
+    }
+
+
 
 }
