fa55af2ff8043884a74b6f87d05b41ddb43c44e7
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-24 19:37:45 +0200

    [PHP] Création de la méthode publicHolidays($year = null) pour retourner tous les jours fériés.

--- src/AppBundle/Manager/VisitManager.php
+++ src/AppBundle/Manager/VisitManager.php
@@ -40,6 +40,7 @@ class VisitManager
     public function initVisit()
     {
         $visit = new Visit();
+        $this->whichVisitDay($visit);
         $this->session->set(self::SESSION_ID_CURRENT_VISIT,$visit);
 
         return $visit;
@@ -86,7 +87,6 @@ class VisitManager
     public function whichVisitDay(Visit $visit)
     {
         // TODO Ajouter les jours fériés
-        // TODO Rajouter la contrainte de validation passée 16h
         date_default_timezone_set('Europe/Paris');
         $hour = date("H:i");
         $today = date("w");
@@ -107,6 +107,7 @@ class VisitManager
         }
 
         $visit->setVisitDate($visitDate);
+        return $visitDate;
     }
 
 
@@ -194,8 +195,51 @@ class VisitManager
         $bookingCode = uniqid();
 
         $visit->setBookingCode($bookingCode);
+        return $bookingCode;
 
     }
+
+
+    public function publicHolidays($year = null)
+    {
+        if ($year === null)
+        {
+            $year = intval(date('Y'));
+        }
+
+        $easterDate  = easter_date($year);
+        $easterDay   = date('d', $easterDate);
+        $easterMonth = date('m', $easterDate);
+        $easterYear   = date('Y', $easterDate);
+
+        $publicHolidays = array(
+            // Dates fixes
+            date('dmY', mktime(0, 0, 0, 1,  1,  $year)), // nouvel an
+            date('dmY', mktime(0, 0, 0, 5,  1,  $year)), // fête du travail
+            date('dmY', mktime(0, 0, 0, 5,  8,  $year)), // 8 mai
+            date('dmY', mktime(0, 0, 0, 7,  14, $year)), // 14 juillet
+            date('dmY', mktime(0, 0, 0, 8,  15, $year)), // 15 août
+            date('dmY', mktime(0, 0, 0, 11, 1,  $year)), // 1e nov
+            date('dmY', mktime(0, 0, 0, 11, 11, $year)), // 11 nov
+            date('dmY', mktime(0, 0, 0, 12, 25, $year)), // noel
+
+            // Dates variables
+            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear)),
+            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear)),
+            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear)),
+        );
+
+        sort($publicHolidays);
+
+        return $publicHolidays;
+    }
+
+
+
+
+
+
+
 
 
 
