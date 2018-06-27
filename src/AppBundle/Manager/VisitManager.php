73d21285f2c39bed46e5d2081336172eb28fe09e
Vanessa Asse <vanessa.asse@gmail.com>
2018-06-27 11:19:15 +0200

    [PHP] Suppression de la méthode PublicHolidays. Les jours fériés sont gérés dans la contrainte NoReservationOnPublicHolidaysValidator. Elle a été ajoutée dans cette classe.

--- src/AppBundle/Manager/VisitManager.php
+++ src/AppBundle/Manager/VisitManager.php
@@ -202,47 +202,6 @@ class VisitManager
     }
 
 
-    public function publicHolidays($year = null)
-    {
-        if ($year === null)
-        {
-            $year = intval(date('Y'));
-        }
-
-        $easterDate  = easter_date($year);
-        $easterDay   = date('d', $easterDate);
-        $easterMonth = date('m', $easterDate);
-        $easterYear   = date('Y', $easterDate);
-
-        $publicHolidays = array(
-            // Dates fixes
-            date('dmY', mktime(0, 0, 0, 1,  1,  $year)), // nouvel an
-            date('dmY', mktime(0, 0, 0, 5,  1,  $year)), // fête du travail
-            date('dmY', mktime(0, 0, 0, 5,  8,  $year)), // 8 mai
-            date('dmY', mktime(0, 0, 0, 7,  14, $year)), // 14 juillet
-            date('dmY', mktime(0, 0, 0, 8,  15, $year)), // 15 août
-            date('dmY', mktime(0, 0, 0, 11, 1,  $year)), // 1e nov
-            date('dmY', mktime(0, 0, 0, 11, 11, $year)), // 11 nov
-            date('dmY', mktime(0, 0, 0, 12, 25, $year)), // noel
-
-            // Dates variables
-            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear)),
-            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear)),
-            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear)),
-        );
-
-        sort($publicHolidays);
-
-        return $publicHolidays;
-    }
-
-
-
-
-
-
-
-
 
 
     //TODO méthode pour empêcher la résa de billets au dela de 1000 billets réservés
