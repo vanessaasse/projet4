856856461f23a3c9c505d7e29336aadb096438ff
Vanessa Asse <vanessa.asse@gmail.com>
2018-07-02 19:27:21 +0200

    [PHP] Modification de la méthode check($day) pour obtenir les jours fériés retournés dans la class PublicHolidaysService

--- src/AppBundle/Validator/Constraints/NoReservationOnPublicHolidaysValidator.php
+++ src/AppBundle/Validator/Constraints/NoReservationOnPublicHolidaysValidator.php
@@ -5,47 +5,22 @@ namespace AppBundle\Validator\Constraints;
 use AppBundle\Manager\VisitManager;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use AppBundle\Service\PublicHolidaysService;
 
 class NoReservationOnPublicHolidaysValidator extends ConstraintValidator
 {
-    public function publicHolidays($year = null)
+    private $publicHolidaysService;
+
+    public function __construct(PublicHolidaysService $publicHolidaysService)
     {
-        if ($year === null)
-        {
-            $year = intval(date('Y'));
-        }
+        $this->publicHolidaysService = $publicHolidaysService;
 
-        $easterDate  = easter_date($year);
-        $easterDay   = date('d', $easterDate);
-        $easterMonth = date('m', $easterDate);
-        $easterYear   = date('Y', $easterDate);
-
-        $publicHolidays = array(
-            // Dates fixes
-            date('dmY', mktime(0, 0, 0, 1,  1,  $year)), // nouvel an
-            date('dmY', mktime(0, 0, 0, 5,  1,  $year)), // fête du travail
-            date('dmY', mktime(0, 0, 0, 5,  8,  $year)), // 8 mai
-            date('dmY', mktime(0, 0, 0, 7,  14, $year)), // 14 juillet
-            date('dmY', mktime(0, 0, 0, 8,  15, $year)), // 15 août
-            date('dmY', mktime(0, 0, 0, 11, 1,  $year)), // 1e nov
-            date('dmY', mktime(0, 0, 0, 11, 11, $year)), // 11 nov
-            date('dmY', mktime(0, 0, 0, 12, 25, $year)), // noel
-
-            // Dates variables
-            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear)),
-            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear)),
-            date('dmY',mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear)),
-        );
-
-        sort($publicHolidays);
-
-        return $publicHolidays;
     }
 
-
-    public function check($day)
+    public function check(\DateTime $day)
     {
-        $publicHolidays = $this->publicHolidays($year = null);
+        $publicHolidays = $this->publicHolidaysService->getPublicHolidays($year = null);
 
         if (in_array($day->format('dmY'), $publicHolidays)) {
             return true;
@@ -66,7 +41,5 @@ class NoReservationOnPublicHolidaysValidator extends ConstraintValidator
             $this->context->buildViolation($constraint->getMessage())
                 ->addViolation();
         }
-
-        dump($value);
     }
 }
