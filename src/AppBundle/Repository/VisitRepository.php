07c10a195da68875f25c9c70a7a7ad87eb699192
Vanessa Asse <vanessa.asse@gmail.com>
2018-07-11 17:40:14 +0200

    [PHP] Ajout de la méthode countNbTicketsOnThisDate($visitDate) pour compter le nombre de billets sur une date

--- src/AppBundle/Repository/VisitRepository.php
+++ src/AppBundle/Repository/VisitRepository.php
@@ -9,5 +9,24 @@ namespace AppBundle\Repository;
  */
 class VisitRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * @param \DateTime $visitDate
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function countNbTicketsOnThisDate($visitDate)
+    {
+
+        $qb = $this->createQueryBuilder('v');
+
+        $qb->select('sum(v.nbTicket)')
+            ->where('v.visitDate = :visitDate')
+            ->setParameter('visitDate', $visitDate);
+
+
+        return $qb
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 
 }
