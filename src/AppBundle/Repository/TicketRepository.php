2cb637fb87b4f6f40bc2b2ff2e6577db3d17118f
Vanessa Asse <vanessa.asse@gmail.com>
2018-07-11 17:37:26 +0200

    [PHP] Suppression de la méthode countNbTicketForDate

--- src/AppBundle/Repository/TicketRepository.php
+++ src/AppBundle/Repository/TicketRepository.php
@@ -14,31 +14,5 @@ use Doctrine\ORM\Mapping;
  */
 class TicketRepository extends \Doctrine\ORM\EntityRepository
 {
-    /*private $repository;
 
-    public function __construct(EntityManager $entityManager)
-    {
-        $this->repository = $entityManager->getRepository(Ticket::class);
-    }*/
-
-    /**
-     * @param \DateTime $visitDate
-     * @return mixed
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function countNbTicketForDate(\Datetime $visitDate)
-    {
-        $qb = $this->createQueryBuilder('t')
-            ->leftJoin('t.visit', 'visit')
-            ->addSelect('visit');
-
-        $qb->select('COUNT(t)')
-            ->where('visit.visitDate = :visitDate')
-            ->setParameter('visitDate', $visitDate);
-
-
-        return $qb
-            ->getQuery()
-            ->getSingleScalarResult();
-    }
 }
