5cb3228ff27f1d23939ab614a8fffc6cd93902e6
Vanessa Asse <vanessa.asse@gmail.com>
2018-07-23 11:25:36 +0200

    [PHP] Création d'une contrainte > impossible d'acheter des billets une fois le nombre des 1000 billets vendus en ligne atteint

--- src/AppBundle/Validator/Constraints/OneThousandTicketsValidator.php
+++ src/AppBundle/Validator/Constraints/OneThousandTicketsValidator.php
@@ -6,7 +6,9 @@ namespace AppBundle\Validator\Constraints;
 use AppBundle\Entity\Visit;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 /**
  * Class OneThousandTicketsValidator
@@ -26,32 +28,30 @@ class OneThousandTicketsValidator extends ConstraintValidator
     }
 
     /**
-     * @param $visitDate
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function checkNbTicketsOnThisDate($visitDate)
-    {
-        $nbTicketsByDay = $this->em->getRepository(Visit::class)->countNbTicketsOnThisDate($visitDate);
-        dump($nbTicketsByDay);
-
-        if($nbTicketsByDay >= 1000) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    /**
      * @param mixed $value
      * @param Constraint $constraint
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function validate($value, Constraint $constraint)
+    public function validate($object, Constraint $constraint)
     {
-        if ($this->checkNbTicketsOnThisDate($value) == true) {
 
+        if(!$constraint instanceof OneThousandTickets){
+            return ;
+        }
+
+        if(!$object instanceof Visit){
+            return ;
+        }
+
+
+        /** @var $visitDate */
+        $visitDate = $object->getVisitDate();
+
+        $total = $this->em->getRepository(Visit::class)->countNbTicketsOnThisDate($visitDate);
+
+        if( $total + $object->getNbTicket() > $constraint->nbTicketsByDay) {
             $this->context->buildViolation($constraint->getMessage())
+                ->atPath('nbTicket')
                 ->addViolation();
         }
     }
