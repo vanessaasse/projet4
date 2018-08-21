938ea1206e5a514fc20e78923334f3a7363c033e
Vanessa Asse <vanessa.asse@gmail.com>
2018-08-21 16:44:04 +0200

    [PHP] Les listeners d'invalidsession en cours de création

---
+++ src/AppBundle/Listener/ExceptionListener.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Listener;
+
+
+use AppBundle\Exception\InvalidVisitSessionException;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Router;
+
+
+class ExceptionListener
+{
+
+    private $router;
+    private $visitManager;
+
+    /**
+     * ExceptionListener constructor.
+     * @param Router $router
+     */
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     * @param VisitManager $visitManager
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+
+        $exception = $event->getException();
+
+        $response = new Response();
+
+
+        if($exception instanceof HttpExceptionInterface) {
+            $this->router->generate('homepage', array('_locale' => 'fr'));
+
+            $event->setResponse(new Response($response));
+        }
+
+
+
+    }
+
+
+}
