32d562fe419f31ab55994e33a4978d2b8976a407
Vanessa Asse <vanessa.asse@gmail.com>
2018-08-23 11:31:22 +0200

    [PHP] Ajout d'un message d'erreur lors de la redirection

--- src/AppBundle/Listener/ExceptionListener.php
+++ src/AppBundle/Listener/ExceptionListener.php
@@ -4,26 +4,32 @@ namespace AppBundle\Listener;
 
 
 use AppBundle\Exception\InvalidVisitSessionException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Router;
-
+use Symfony\Component\Translation\TranslatorInterface;
 
 
 class ExceptionListener
 {
     private $router;
 
+    private $session;
+    private $translator;
+
     /**
      * ExceptionListener constructor.
      * @param Router $router
      */
-    public function __construct(Router $router)
+    public function __construct(Router $router, SessionInterface $session, TranslatorInterface $translator)
     {
         $this->router = $router;
+        $this->session = $session;
+        $this->translator = $translator;
     }
 
     /**
@@ -34,7 +40,10 @@ class ExceptionListener
         $exception = $event->getException();
 
         if($exception instanceof InvalidVisitSessionException) {
+
             $url = $this->router->generate('homepage');
+            $message = $this->translator->trans('exception.message_error');
+            $this->session->getFlashBag()->add('error', $message);
             $event->setResponse(new RedirectResponse($url));
         }
 
